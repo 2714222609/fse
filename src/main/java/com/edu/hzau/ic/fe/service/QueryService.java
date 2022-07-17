4a9279de5b3a630bf399b623c4c24418281f438c
Yue Tang <2714222609@qq.com>
2022-07-17 21:21:35 +0800

    引入redis缓存优化，替换json工具为fastjson

---
+++ src/main/java/com/edu/hzau/ic/fe/service/QueryService.java
@@ -0,0 +1,50 @@
+package com.edu.hzau.ic.fe.service;
+
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.edu.hzau.ic.fe.utils.DateUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @Author yue
+ */
+@Slf4j
+@Service
+public class QueryService {
+    @Autowired
+    FDAFunctions fdaFunctions;
+    @Autowired
+    DateUtils dateUtil;
+    @Autowired
+    private RedisService redisService;
+
+    public JSONArray query(@RequestBody JSONObject jsonObject) {
+        // response time
+        double startTime = dateUtil.getDate();
+        double endTime;
+
+        // 查缓存
+        String datalog = (String) jsonObject.get("datalog");
+        JSONArray resultJson;
+        String key = String.valueOf(datalog.hashCode());
+        String resultString = redisService.getString(key);
+        if (resultString != null) {
+            resultJson = JSONArray.parseArray(resultString);
+        }else {
+            log.info("Query cache missed.");
+            resultJson = fdaFunctions.query(datalog);
+            // 缓存到redis
+            resultString = resultJson.toString();
+            redisService.setString(key, resultString);
+        }
+        endTime = dateUtil.getDate();
+        double responseTime = (endTime - startTime) / 1000;
+        JSONObject responseTimeJson = new JSONObject();
+        responseTimeJson.put("response time", responseTime + "s");
+        resultJson.add(responseTimeJson);
+        return resultJson;
+    }
+}
